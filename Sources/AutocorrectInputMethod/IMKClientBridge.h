818a9ac3192c094a21553b6859d3cb94f9714f9c
Angelo David <90495108+notsonata@users.noreply.github.com>
2026-08-28 14:46:00 +0800

    build: stabilize Swift names for IMK bridge

--- Sources/AutocorrectInputMethod/IMKClientBridge.h
+++ Sources/AutocorrectInputMethod/IMKClientBridge.h
@@ -4,9 +4,14 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface IMKClientBridge : NSObject
 
-+ (NSRange)selectedRangeForClient:(id)client;
-+ (nullable NSString *)stringForRange:(NSRange)range client:(id)client;
-+ (void)insertText:(NSString *)text replacingRange:(NSRange)range client:(id)client;
++ (NSRange)selectedRangeForClient:(id)client
+    NS_SWIFT_NAME(selectedRange(client:));
+
++ (nullable NSString *)stringForRange:(NSRange)range client:(id)client
+    NS_SWIFT_NAME(string(range:client:));
+
++ (void)insertText:(NSString *)text replacingRange:(NSRange)range client:(id)client
+    NS_SWIFT_NAME(insert(text:replacing:client:));
 
 @end
 
